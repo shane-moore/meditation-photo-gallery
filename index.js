b3a227092bb2fef6aca8b714748bfd8f7690be48
shane-moore <skm1790@gmail.com>
2020-02-23 15:48:48 -0600

    successfully used imagemin

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
-
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+ 
 (async () => {
-	const files = await imagemin(['images/*.{jpg,png}'], {
-		destination: 'build/images',
-		plugins: [
-			imageminJpegtran(),
-			imageminPngquant({
-				quality: [0.6, 0.8]
-			})
-		]
-	});
-
-	console.log(files);
-	//=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
+  const files = await imagemin(["images/*.jpg"], {
+    destination: "compressed-images",
+    plugins: [
+      imageminMozjpeg({quality: 50})
+    ]
+  });
 })();
